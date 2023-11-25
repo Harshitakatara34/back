9f50cf3727ee73e65ca96257ab484664b9d5f477
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-11-25 05:49:54 +0530

    done

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const mongoURL = process.env.mongoURL;
 
-const connection = mongoose.connect("mongodb+srv://Harshita:harshita@cluster0.g1zjwi1.mongodb.net/Zura?retryWrites=true&w=majority");
+const connection = mongoose.connect(process.env.mongoURL);
 
 module.exports = { connection };
