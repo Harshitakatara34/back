935ac61c916947027dc314275e6148c3bec37dab
Harshita Katara <112870595+Harshitakatara34@users.noreply.github.com>
2023-11-26 20:32:00 +0530

    done

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { UserModel } = require("../Model/UserModel");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const userRouter = express.Router();
-const secretKey = process.env.secretKey;
+
 // creating and checking for a user
 userRouter.post("/", async (req, res) => {
   try {
@@ -12,7 +11,7 @@ userRouter.post("/", async (req, res) => {
     // Check if the user already exists with the provided email
     const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
-      const token = jwt.sign({ email }, secretKey, { expiresIn: "8h" });
+      const token = jwt.sign({ email }, "harshi", { expiresIn: "8h" });
       res.status(201).send({
         msg: "User already exists, and  Logged in successfully",
         token,
@@ -23,7 +22,7 @@ userRouter.post("/", async (req, res) => {
 
     // Create a new user if it doesn't exist
     const newUser = await UserModel.create({ email });
-    const tokenAtSignup = jwt.sign({ email }, secretKey, { expiresIn: "8h" });
+    const tokenAtSignup = jwt.sign({ email }, "harshi", { expiresIn: "8h" });
 
     res.status(201).send({
       msg: "New User Created",
